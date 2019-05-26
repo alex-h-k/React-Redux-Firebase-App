fb143ddded9b66a6659e84c7653d0d5155837f50
alex-h-k <kuan8@hotmail.com>
2019-05-26 17:51:30 +1200

    create firebase project

---
+++ marioplan/src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyClD-SDbteCBuHujsz0dNsZDakrJFo6Uzw",
+  authDomain: "net-ninja-marioplan-5de0b.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-5de0b.firebaseio.com",
+  projectId: "net-ninja-marioplan-5de0b",
+  storageBucket: "net-ninja-marioplan-5de0b.appspot.com",
+  messagingSenderId: "96564704704",
+  appId: "1:96564704704:web:0b2be22d6081e0dc"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;

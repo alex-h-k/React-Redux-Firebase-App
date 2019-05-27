172dea19305741327d52013475e99883401722d9
alex-h-k <kuan8@hotmail.com>
2019-05-28 11:42:22 +1200

    syncing data with Firestore

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import Notifications from "./Notifications";
 import ProjectList from "../project/ProjectList";
+import Notifications from "./Notifications";
 import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends Component {
   render() {
+    // console.log(this.props);
     const { projects } = this.props;
+
     return (
       <div className="dashboard container">
         <div className="row">
@@ -20,10 +24,15 @@ class Dashboard extends Component {
     );
   }
 }
-const mapStatesToProps = state => {
+
+const mapStateToProps = state => {
+  console.log(state);
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   };
 };
 
-export default connect(mapStatesToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(Dashboard);

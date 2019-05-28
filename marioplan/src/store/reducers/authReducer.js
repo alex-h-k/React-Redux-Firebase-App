8ceb894868705f592356d0068062120aab2a8be7
alex-h-k <kuan8@hotmail.com>
2019-05-28 14:16:50 +1200

    update authReducer

--- marioplan/src/store/reducers/authReducer.js
+++ marioplan/src/store/reducers/authReducer.js
@@ -1,7 +1,24 @@
-const initState = {};
+const initState = {
+  anthError: null
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("login error");
+      return {
+        ...state,
+        authError: "login failed"
+      };
+    case "LOGIN_SUCESS":
+      console.log("login sucess");
+      return {
+        ...state,
+        anthError: null
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;

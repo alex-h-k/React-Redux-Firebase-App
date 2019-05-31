97cd95d5996b3cf8d7d9ad9f9540ca71e84c1707
alex-h-k <kuan8@hotmail.com>
2019-05-31 14:11:53 +1200

    redirect to home page after create new project

--- marioplan/src/components/project/CreateProject.js
+++ marioplan/src/components/project/CreateProject.js
@@ -16,6 +16,7 @@ class CreateProject extends Component {
     e.preventDefault();
     this.props.createProject(this.state);
     // console.log(this.state);
+    this.props.history.push("/");
   };
   render() {
     const { auth } = this.props;

9c87c96b085b1bf9b635f2cb9c819b4b65dd7b3c
alex-h-k <kuan8@hotmail.com>
2019-06-10 14:25:45 +1200

    deploy app to firebase

---
+++ marioplan/dist/precache-manifest.84cc02ac7e79045886e10ca905f4b670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f29eeae47d2f9081ca",
+    "url": "/static/css/main.5f6a13c7.chunk.css"
+  },
+  {
+    "revision": "32f29eeae47d2f9081ca",
+    "url": "/static/js/main.847a43a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "65a2cab92cb33e2b4ed4",
+    "url": "/static/js/2.b7e4dfa0.chunk.js"
+  },
+  {
+    "revision": "e0fbcce22177bc1cfb8cb2053e9618e9",
+    "url": "/index.html"
+  }
+];
